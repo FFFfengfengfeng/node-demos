e6c0f53e4200d437991410d86dc8f3e6cec8236c
FFF <1098865109@qq.com>
2018-12-29 13:29:18 +0800

    自定义组件

---
+++ src/app/goods-list/goods-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goods-list',
+  template: `
+    <p>
+      goods-list works!
+    </p>
+  `,
+  styles: []
+})
+export class GoodsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
