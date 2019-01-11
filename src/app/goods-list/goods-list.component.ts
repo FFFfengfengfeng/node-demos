44f8a97d5900ba594daaf2fb352d89a9932a51e8
FFF <1098865109@qq.com>
2019-01-11 14:28:28 +0800

    使用自定义组件,定义接口

--- src/app/goods-list/goods-list.component.ts
+++ src/app/goods-list/goods-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { goods } from '../interface/goods';
 
 @Component({
     selector: 'app-goods-list',
@@ -6,8 +7,48 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./goods-list.component.less']
 })
 export class GoodsListComponent implements OnInit {
+    list: Array<goods>
 
-    constructor() { }
+    constructor() {
+        this.list = [
+            {
+                title: '商品1',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+            {
+                title: '商品2',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+            {
+                title: '商品3',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+            {
+                title: '商品4',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+            {
+                title: '商品5',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+            {
+                title: '商品6',
+                intro: '商品简介',
+                time: '2019/01/11',
+                pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547197740565&di=b5d7d7caae1aa1de698ca79d2aef6df8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa50f4bfbfbedab648eab1f7cf736afc378311eb4.jpg'
+            },
+        ];
+    }
 
     ngOnInit() {
         
---
+++ src/app/interface/goods.ts
@@ -0,0 +1,6 @@
+export interface goods {
+    title: string,
+    intro: string,
+    time: string,
+    pic: string
+}
