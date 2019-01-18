a61e48515dbb0e11c75c12e110bc612268d53ec7
FFF <1098865109@qq.com>
2019-01-18 16:50:50 +0800

    删除app.component.ts内容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-    name: string;
-    list: Array<object>;
-    skills: Array<string>;
-    showName: boolean;
-    toggleShow: Function;
-    addSkill: Function;
-
-    constructor() {
-        this.name = 'FFF';
-        this.list = [
-            {
-                name: 'FFF',
-                sex: '男'
-            },
-            {
-                name: 'ZZZ',
-                sex: '女'
-            }
-        ];
-        this.skills = ['音乐', '篮球'];
-        this.showName = false;
-        this.toggleShow = () => {
-            this.showName = !this.showName;
-        },
-        this.addSkill = (skill: string)=> {
-            const _this = this;
-
-            _this.skills.push(skill);
-        }
-    }
+    
 }
